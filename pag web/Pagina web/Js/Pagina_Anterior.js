34a6bd6beed947af3b4b1676afef02d9987bfa57
Thiago <nabonithiago@gmail.com>
2026-09-14 08:08:24 -0300

    Fix carpetas
    las acomode

--- pag web/Pagina web/Js/Pagina_Anterior.js
+++
@@ -1,7 +0,0 @@
-function volverPaginaAnterior() {
-    if (document.referrer) {
-        window.location.href = document.referrer;
-    } else {
-        window.location.href = "index.html";
-    }
-}
