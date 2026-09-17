d72efb45fd02285a498d6d049d408fdf897837aa
Crack555titi <nabonithiago@gmail.com>
2026-09-16 21:38:01 -0300

    Update Pagina_Anterior.js

--- Pagina web/Js/Pagina_Anterior.js
+++ Pagina web/Js/Pagina_Anterior.js
@@ -2,6 +2,6 @@ function volverPaginaAnterior()
 {
     if (document.referrer) 
     {
-        window.location.href = "/Pagina%20web/index.html";
+        window.location.href = "/Pagina%20web";
     }
 }
